98f551e85c180a241619f4f0fb4f6fc6528c14bc
Çağrı ÇAĞMAN <cagri.cagman@gmail.com>
2018-01-11 05:49:25 -0800

    giriş yapma fonksiyonu eklendi

--- assets/js/app/login.js
+++ assets/js/app/login.js
@@ -1,3 +1,24 @@
 $(document).ready(function() {
-	alert();
+	var config = {
+		apiKey: "AIzaSyCPOQs6HjTCv64O_0tuJfYGqM_kpZ1veaI",
+		authDomain: "etkinlik-takip.firebaseapp.com",
+		databaseURL: "https://etkinlik-takip.firebaseio.com",
+		projectId: "etkinlik-takip",
+		storageBucket: "etkinlik-takip.appspot.com",
+		messagingSenderId: "944567601766"
+	};
+	firebase.initializeApp(config);
+
+	$("#loginBtn").click(function() {
+
+		var email=$("#email").val();
+		var password=$("#password").val();
+
+		firebase.auth().signInWithEmailAndPassword(email, password)
+		.then(function(){
+			alert();
+		}).catch(function (error) {
+			alert(error.message);
+		})
+	});
 });
