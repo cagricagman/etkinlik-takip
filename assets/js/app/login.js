b98ab2f4a55c219bbd499ecba250efd9fa99d848
Çağrı ÇAĞMAN <cagri.cagman@gmail.com>
2018-01-12 01:18:23 +0300

    Güncellemeler yapıldı

--- assets/js/app/login.js
+++ assets/js/app/login.js
@@ -1,24 +1,21 @@
-$(document).ready(function() {
-	var config = {
-		apiKey: "AIzaSyCPOQs6HjTCv64O_0tuJfYGqM_kpZ1veaI",
-		authDomain: "etkinlik-takip.firebaseapp.com",
-		databaseURL: "https://etkinlik-takip.firebaseio.com",
-		projectId: "etkinlik-takip",
-		storageBucket: "etkinlik-takip.appspot.com",
-		messagingSenderId: "944567601766"
-	};
-	firebase.initializeApp(config);
+$(document).ready(function () {
+  var config = {
+      apiKey: "AIzaSyCPOQs6HjTCv64O_0tuJfYGqM_kpZ1veaI",
+      authDomain: "etkinlik-takip.firebaseapp.com",
+      databaseURL: "https://etkinlik-takip.firebaseio.com",
+      projectId: "etkinlik-takip",
+      storageBucket: "etkinlik-takip.appspot.com",
+      messagingSenderId: "944567601766"
+    };
+    firebase.initializeApp(config);
 
-	$("#loginBtn").click(function() {
-
-		var email=$("#email").val();
-		var password=$("#password").val();
-
-		firebase.auth().signInWithEmailAndPassword(email, password)
-		.then(function(){
-			alert();
-		}).catch(function (error) {
-			alert(error.message);
-		})
-	});
-});
+    $("#loginBtn").click(function () {
+      var email = $("#email").val();
+      var password = $("#password").val();
+      firebase.auth().signInWithEmailAndPassword(email, password).then(function () {
+        window.location.href = "index.html";
+      }).catch(function(error){
+        alert(error.message);
+      })
+    })
+})
